86c01ab496aeb9882b1f2d62cee89ad188fed37d
Татьяна Анисимова <kulmina@mail.ru>
2021-11-22 20:32:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = random.Next(0, 10);
+                Console.Write("{0} ", array[i]);
+
+                string path = "Tanya.txt";
+                if (!File.Exists(path))
+                {
+                    File.Create(path);
+                }
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("array[]", array);
+                }
+                
+
+            }
+        }
+
+    }
+
+}
